84b364e36b8e4eba80df54bcbc06fc497f7cb01a
parseb <petra306@protonmail.com>
2022-05-22 00:12:10 +0300

    basic function walkthrough front

--- js/main.js
+++ js/main.js
@@ -1017,7 +1017,10 @@ const ERC20Abi = [
       "name": "Transfer",
       "type": "event"
   }
-] 
+] ;
+
+const UniV2Abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1","type":"uint256"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"Burn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0In","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1In","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount0Out","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1Out","type":"uint256"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"Swap","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint112","name":"reserve0","type":"uint112"},{"indexed":false,"internalType":"uint112","name":"reserve1","type":"uint112"}],"name":"Sync","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"MINIMUM_LIQUIDITY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"PERMIT_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"burn","outputs":[{"internalType":"uint256","name":"amount0","type":"uint256"},{"internalType":"uint256","name":"amount1","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"factory","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getReserves","outputs":[{"internalType":"uint112","name":"_reserve0","type":"uint112"},{"internalType":"uint112","name":"_reserve1","type":"uint112"},{"internalType":"uint32","name":"_blockTimestampLast","type":"uint32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_token0","type":"address"},{"internalType":"address","name":"_token1","type":"address"}],"name":"initialize","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"kLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"mint","outputs":[{"internalType":"uint256","name":"liquidity","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"price0CumulativeLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"price1CumulativeLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"skim","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount0Out","type":"uint256"},{"internalType":"uint256","name":"amount1Out","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"swap","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"sync","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"token0","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"token1","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}];
+
 // Unpkg imports
 const Web3Modal = window.Web3Modal.default;
 const WalletConnectProvider = window.WalletConnectProvider.default;
@@ -1126,6 +1129,8 @@ const daicreate = document.getElementById("dai-create");
 
 const floatingcowsplace = document.getElementById("floating-cows-place");
 const mintcowbutton = document.getElementById("mint-cow-button");
+const vestingsbox = document.getElementById("vestingbox");
+
 
 /**
  * Kick in the UI action after Web3modal dialog has chosen a provider
@@ -1213,7 +1218,7 @@ async function retrieveAllCows(C_contract) {
     document.getElementById("cowlisttable").innerHTML += `
     <tr>
     <th scope="row">${index+1}</th>
-    <td>${index}</td>
+    <td>${cow[0][0]}</td>
     <td>${cow[0][1]}</td>
     <td>${cow[0][2]}</td>
     <td>${cow[0][3]}</td>
@@ -1268,7 +1273,7 @@ async function getFloatingCows(cowContract){
       Vest/Liquidate in Days <br><span id="vest-liq">${c.vestStartEnd[0]} </span></div>
     
     <div class="col-3 text-center">
-      Fully vests by <br> <span id="vest-by">${c.vestStartEnd[1]}</span></div>
+      Fully vests in (days) <br> <span id="vest-by">${c.vestStartEnd[1]}</span></div>
     
     <div class="col-3 text-center">
       <a href="http://ipfs.io" class="text-decoration-none"> pitch deck link #IPFS </a>
@@ -1350,7 +1355,6 @@ async function submittedCreateCow(){
   console.log("enw cow tx", tx);
   tx.then( (tx) => {
     console.log("tx", tx);
-    alert("tx", tx);
   });
 }
 
@@ -1368,6 +1372,74 @@ async function takeMintDeal(_dealId) {
 
 }
 
+async function getPoolReserves(poolAddress) {
+  let poolContract = new state.web3.eth.Contract(UniV2Abi, poolAddress);
+  let reserves = await poolContract.methods.getReserves().call();
+  return reserves;
+}
+
+async function appendVestableCows() {
+  state.allCows.forEach( async (c) => {
+    let id = state.allCows.indexOf(c) + 1;
+    console.log("vestings", c)
+    if (c.owners[1] == selectedAccount ) {
+      let vestAppend = `
+      <div class="row">
+       <div class="col-8"> 
+       <div class="p-3 border bg-light text-center">
+        <b> Pool Reserves </b> <br />
+        <span id="yp-price-now"> </span> <br />
+       </div>
+       <div class="p-3 border bg-light text-center">
+       <b> Profit/Loss </b> <br />
+       4444
+      </div>
+       </div>
+       <div class="col-4 text-center">
+       <div class="row p-1">
+          <button class="btn btn-outline-dark btn-block" onclick="liquidateCow(${id})">Liquidate</button><br>
+        </div>
+        <div class="row p-1">
+          <button class="btn btn-outline-dark  btn-block" onclick="vestCow(${id})">Vest</button><br>
+        </div>
+        <div class="row p-1">
+          <button class="btn btn-outline-dark btn-block" onclick="liquidateCow(${id})">Withdraw Available</button><br>
+        <div>
+        </div>
+      </div>
+      <br>
+      <hr>
+      </div>
+      </div>
+      <div class="row">
+      <div class="col-12"> <div class="input-group mb-3">
+      <input type="text" id="transferto" class="form-control" placeholder="transfer to 0xaddressofrecipient" aria-label="Transfer to:" aria-describedby="transfer">
+      <button class="btn btn-outline-danger" onclick="transferNFT(${id})" type="button" id="button-addon2">Transfer</button>
+    </div>
+      </div>
+      `;
+      vestingsbox.innerHTML += vestAppend;
+      let priceNowSpan = document.getElementById("yp-price-now");
+      priceNowSpan.innerText = "loading...";
+      let reserves = await getPoolReserves(c.owners[3]);
+      priceNowSpan.innerText = `${reserves[0]} | ${reserves[1]} | last touched: ${reserves[2]}`;
+      console.log(c);
+    }
+  })
+}
+
+async function transferNFT(_nftId) { 
+  const ethersProvider =  new ethers.providers.Web3Provider(window.ethereum) 
+  let signer = ethersProvider.getSigner();
+  let cowC = new ethers.Contract(state.cowAddress, CCowAbi, ethersProvider);
+  let signedCow = cowC.connect(signer);
+
+  let tx =  signedCow.transferFrom(selectedAccount, document.getElementById("transferto").value, _nftId);
+  
+  tx.then( (tx) => {
+    console.log("tx", tx);
+  });
+}
 
 
 async function getDisplayData(){}
